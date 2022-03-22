6896e3530311087f1997b8fe947b07029ffbd0ef
sureshgans <suresh.ganesan@hitachivantara.com>
2022-03-22 11:49:04 -0400

    Updating azure backend

--- backend.tf
+++ backend.tf
@@ -6,14 +6,3 @@ terraform {
     key                  = "infra.tfstate"
   }
 }
-
-data "terraform_remote_state" "rg" {
-  backend = "azurerm"
-
-  config = {
-    resource_group_name  = "terraform-state-file-rg"
-    storage_account_name = "devops032022sa"
-    container_name       = "tfstate"
-    key                  = "rg.tfstate"
-  }
-}
